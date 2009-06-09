f86f96adb1f7bce294a63e222e8d97419bb3042a
Fadhli Rahim <fadhlirahim@gmail.com>
2009-06-10 03:25:28 +1000

    Rename to event.starts_at & event.ends_at

--- lib/weekly_calendar.rb
+++ lib/weekly_calendar.rb
@@ -62,10 +62,10 @@ module WeeklyHelper
             concat(tag("div", :id => day_row))
               hours.each do |h|
                 for event in @objects
-                  if event.start_date.strftime('%j').to_s == day.strftime('%j').to_s
-                    if event.start_date.strftime('%H').to_i >= start_hour and event.end_date.strftime('%H').to_i <= end_hour
-                      concat(tag("div", :id => "week_event", :style =>"left:#{left(event.start_date,options[:business_hours])}px;width:#{width(event.start_date,event.end_date)}px;", :onclick => "location.href='/events/#{event.id}';"))
-                      truncate = truncate_width(width(event.start_date,event.end_date))
+                  if event.starts_at.strftime('%j').to_s == day.strftime('%j').to_s
+                    if event.starts_at.strftime('%H').to_i >= start_hour and event.ends_at.strftime('%H').to_i <= end_hour
+                      concat(tag("div", :id => "week_event", :style =>"left:#{left(event.starts_at,options[:business_hours])}px;width:#{width(event.starts_at,event.ends_at)}px;", :onclick => "location.href='/events/#{event.id}';"))
+                      truncate = truncate_width(width(event.starts_at,event.ends_at))
                       yield(event,truncate)
                       concat("</div>")
                     end
