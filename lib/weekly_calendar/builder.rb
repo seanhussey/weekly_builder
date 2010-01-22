dcec77fd2d094e47a8b5501ee9340842a08ce175
Sean Hussey <sean@seanhussey.com>
2010-01-22 14:07:23 -0500

    Reformatting cleanup.

--- lib/weekly_calendar/builder.rb
+++ lib/weekly_calendar/builder.rb
@@ -22,19 +22,19 @@ class WeeklyCalendar::Builder
   def week(options = {})    
     days
     if options[:business_hours] == "true" or options[:business_hours].blank?
-      hours = ["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm"]
+      hours      = ["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm"]
       header_row = "header_row"
-      day_row = "day_row"
-      grid = "grid"
+      day_row    = "day_row"
+      grid       = "grid"
       start_hour = 6
-      end_hour = 20
+      end_hour   = 20
     else
-      hours = ["1am","2am","3am","4am","5am","6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm","9pm","10pm","11pm","12am"]
+      hours      = ["1am","2am","3am","4am","5am","6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm","9pm","10pm","11pm","12am"]
       header_row = "full_header_row"
-      day_row = "full_day_row"
-      grid = "full_grid"
+      day_row    = "full_day_row"
+      grid       = "full_grid"
       start_hour = 1
-      end_hour = 24
+      end_hour   = 24
     end
     
     concat(tag("div", :id => "hours"))
@@ -73,21 +73,21 @@ class WeeklyCalendar::Builder
     def left(starts_at,business_hours)
       if business_hours == "true" or business_hours.blank?
         minutes = starts_at.strftime('%M').to_f * 1.25
-        hour = starts_at.strftime('%H').to_f - 6
+        hour    = starts_at.strftime('%H').to_f - 6
       else
         minutes = starts_at.strftime('%M').to_f * 1.25
-        hour = starts_at.strftime('%H').to_f
+        hour    = starts_at.strftime('%H').to_f
       end
       left = (hour * 75) + minutes
     end
 
     def width(starts_at,ends_at)
       #example 3:30 - 5:30
-      start_hours = starts_at.strftime('%H').to_i * 60 # 3 * 60 = 180
+      start_hours   = starts_at.strftime('%H').to_i * 60 # 3 * 60 = 180
       start_minutes = starts_at.strftime('%M').to_i + start_hours # 30 + 180 = 210
-      end_hours = ends_at.strftime('%H').to_i * 60 # 5 * 60 = 300
-      end_minutes = ends_at.strftime('%M').to_i + end_hours # 30 + 300 = 330
-      difference =  (end_minutes.to_i - start_minutes.to_i) * 1.25 # (330 - 180) = 150 * 1.25 = 187.5
+      end_hours     = ends_at.strftime('%H').to_i * 60 # 5 * 60 = 300
+      end_minutes   = ends_at.strftime('%M').to_i + end_hours # 30 + 300 = 330
+      difference    = (end_minutes.to_i - start_minutes.to_i) * 1.25 # (330 - 180) = 150 * 1.25 = 187.5
     
       unless difference < 60
         width = difference - 12
@@ -97,7 +97,7 @@ class WeeklyCalendar::Builder
     end
   
     def truncate_width(width)
-      hours = width / 63
+      hours          = width / 63
       truncate_width = 20 * hours
     end
     
