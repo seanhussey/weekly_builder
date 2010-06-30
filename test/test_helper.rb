15fbd998c8503ef79eb498205fd18ab412eeb651
Sean Hussey <sean@seanhussey.com>
2010-06-30 10:42:26 -0400

    Fixing test_helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,9 +6,9 @@ require 'action_pack'
 require 'action_controller'
 require 'action_view'
 require 'action_controller/test_process'
-require "shoulda"
-require "mocha"
+require 'shoulda'
+require 'mocha'
 
-require '../lib/weekly_calendar'
+require 'lib/weekly_calendar'
 
 require(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'weekly_calendar/builder')))
